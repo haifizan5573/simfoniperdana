a3afd5fae0730e4148c54a2ba3543e0858ea0817
Mohd haifizan Mohd raya <mohdhaifizanmohdraya@MacBook-Pro-Mohd.local>
2022-02-12 07:39:39 +0800

    Update

---
+++ database/seeders/AddRolePermissionAdmin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class AddRolePermission extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //add new permission
+        $permissions = [
+            'app-list',
+            'app-create',
+            'app-edit',
+            'app-delete',
+            'app-assign',
+            'admin',
+            'public',
+         ];
+       
+
+         //sync to role
+        $role = Role::where('name','Admin')->first();
+     
+        $perms= Permission::whereIn('name',$permissions)->pluck('id','id')->ToArray();
+        //dd($perms);
+        $role->syncPermissions($perms);
+    }
+}
