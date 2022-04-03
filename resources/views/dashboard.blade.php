92f522135dc12bb8d0e94a27fd76f2257c0c6963
Mohd haifizan Mohd raya <mohdhaifizanmohdraya@MacBook-Pro-Mohd.local>
2022-04-03 17:52:38 +0800

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,49 +2,15 @@
 
 @section('content')
 
+   
 <div class="row">
-    <div class="col-md-3">
-            @include('layouts.components.card_block',[
-                             'icon'=>'mdi mdi-bullseye-arrow mdi-lg',
-                             'bgcolor'=>'bg-dark bg-gradient',
-                             'title'=>"0 Application",
-                             'subtitle'=>"&nbsp;",
-                             'content'=>'Today Submission',
 
-                            ])
-    </div>
-    <div class="col-md-3">
-             @include('layouts.components.card_block',[
-                             'icon'=>'dripicons-blog',
-                             'bgcolor'=>'bg-success bg-gradient',
-                             'title'=>"0 Application",
-                             'subtitle'=>"RM0",
-                             'content'=>'Total Disbursed - '.$curMonth,
+        @include('livewire.dashboard.upcomingevents')
 
-                            ])
+        @include('livewire.dashboard.notifications')
 
-    </div>
-    <div class="col-md-3">
-
-            @include('layouts.components.card_block',[
-                             'icon'=>'dripicons-blog',
-                             'bgcolor'=>'bg-danger bg-gradient',
-                             'title'=>"&nbsp;",
-                             'subtitle'=>"RM0",
-                             'content'=>'Pending Application',
-
-                            ])
-    </div>
-    <div class="col-md-3">
-             @include('layouts.components.card_block',[
-                             'icon'=>'dripicons-blog',
-                             'bgcolor'=>'bg-primary bg-gradient',
-                             'title'=>"&nbsp;",
-                             'subtitle'=>"RM0",
-                             'content'=>'Latest Announcement',
-
-                            ])
-    </div>
-
+        @include('livewire.dashboard.upcomingevents')
+    
 </div>
+                 
 @endsection
---
+++ resources/views/livewire/dashboard/notifications.blade.php
@@ -0,0 +1,8 @@
+<div class="col-xl-4">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title mb-4">Notifications</h4>
+
+        </div>
+    </div>
+</div>
