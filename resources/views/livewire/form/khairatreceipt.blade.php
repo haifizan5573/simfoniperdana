619e0a05e2b30111a8fc719d5c8ca77d3c282af9
Mohd haifizan Mohd raya <mohdhaifizanmohdraya@MacBook-Pro-Mohd.local>
2022-04-03 12:31:49 +0800

    update

--- resources/views/livewire/form/khairatreceipt.blade.php
+++ resources/views/livewire/form/khairatreceipt.blade.php
@@ -2,20 +2,19 @@
 
   @if(!empty($dataid))
         
-        {{$data->getMime($data->path)}}
-        {{$data->link}}
+       
         @if($data->getMime($data->path)=="application/pdf")
             
-          <iframe src="{{ env('APP_URL')}}/{{ $data->link }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
+          <iframe src="{{ env('APP_URL')}}/{{ $data['link'] }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
 
          @elseif($data->getMime($data->path)=="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
 
-            <iframe src="https://view.officeapps.live.com/op/view.aspx?src={{ env('APP_URL')}}/{{ $data->link }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
+            <iframe src="https://view.officeapps.live.com/op/view.aspx?src={{ env('APP_URL')}}/{{ $data['link'] }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
 
         @else
 
               
-            <iframe src="{{ route('viewattachment',['id'=>$data->link ]) }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
+            <iframe src="{{ route('viewattachment',['id'=>$data['link'] ]) }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
                                                         
         @endif
 @else
