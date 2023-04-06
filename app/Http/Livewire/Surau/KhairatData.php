2d444c4b50851d2e878326220efdbab45dead1ba
Haifizan <switch@switchs-MacBook-Pro.local>
2023-04-06 17:19:14 +0800

    update

--- app/Http/Livewire/Surau/KhairatData.php
+++ app/Http/Livewire/Surau/KhairatData.php
@@ -170,7 +170,7 @@ class KhairatData extends Component
 
     public function editstatus(){
 
-        dd($this->status);
+        dd($this->userid);
 
         if(!empty($this->status)){
             $khairatuser=KhairatUser::where('userid',$this->userid)->first();
